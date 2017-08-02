6871809275f689a21db1ac9ae953900948858ca9
Roy Shea <royshea@gmail.com>
2017-08-01 21:12:22 -0700

    hope: Skeleton of a puzzle.

---
+++ Practice/ANewHope.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+{
+    public class ANewHope
+    {
+        public static int count(int[] firstWeek, int[] lastWeek, int days)
+        {
+            // Iterate through first week tracking index for each key
+
+            // Iterate through second week tracking the spread from that key in first week
+
+            // Find largest spread
+
+            // Use this spread to determine how long it takes to shift shirst-
+            return 4;
+        }
+    }
+}
