c1b21247dec567e56d8030bb9a711ec7efe6e38b
Roy Shea <royshea@gmail.com>
2017-08-02 19:27:14 -0700

    anewhope: Basic solution

--- Practice/ANewHope.cs
+++ Practice/ANewHope.cs
@@ -1,21 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Linq;
 
 namespace Practice
 {
     public class ANewHope
     {
-        public static int count(int[] firstWeek, int[] lastWeek, int days)
+        public static int count(int[] firstWeek, int[] lastWeek, int washDays)
         {
+            // Basic properties
+            var weekLength = firstWeek.Length;
+            var maxMove = weekLength - washDays;
+
             // Iterate through first week tracking index for each key
+            var deltas = new int[weekLength];
+            for (var i = 0; i < weekLength; i++)
+            {
+                var shirtId = firstWeek[i];
+                deltas[shirtId-1] = i;
+            }
 
             // Iterate through second week tracking the spread from that key in first week
+            for (var i = 0; i < weekLength; i++)
+            {
+                var shirtId = lastWeek[i];
+                deltas[shirtId-1] -= i;
+            }
 
             // Find largest spread
+            var largestMoveNeeded = deltas.Max();
 
-            // Use this spread to determine how long it takes to shift shirst-
-            return 4;
+            // Use this spread to determine how long it takes to shift
+            //
+            // Extra math in this implements ceil of largestMoveNeeded / maxMove in int math.
+            var numWeeks = (largestMoveNeeded + maxMove - 1) / maxMove;
+
+            // Account for starting point
+            var initialWeek = 1;
+
+            return numWeeks + initialWeek;
         }
     }
 }
--- TestPractice/ANewHopeTest.cs
+++ TestPractice/ANewHopeTest.cs
@@ -14,6 +14,20 @@ namespace TestPractice
                 new int[] { 1, 2, 3, 4 },
                 new int[] { 4, 3, 2, 1 },
                 3);
+            // Find the minimum move required.  This is (?) the limiting factor.
+            // 1: 0 -> 3 .. 0 - 3 = -3
+            // 2: 1 -> 2 .. 1 - 2 = -1
+            // 3: 2 -> 1 .. 2 - 1 =  1
+            // 4: 3 -> 0 .. 3 - 0 =  3
+            //
+            // How far can a shirt move each week?
+            // (4_week - 3_wash) = 1_move
+            //
+            // Must have enough weeks (after the first) to be at least equal to the move
+            // 1_move * weeks >= 3
+            // weeks >= 3 / 1 == 3
+            //
+            // 3 weeks after first means 3 + 1 weeks for 4
             Assert.AreEqual(4, numWeeks);
         }
 
@@ -23,6 +37,15 @@ namespace TestPractice
             var numWeeks = ANewHope.count(
                 new int[] { 8, 5, 4, 1, 7, 6, 3, 2 },
                 new int[] { 2, 4, 6, 8, 1, 3, 5, 7 },
+                // 1: 3 - 5 = -3 
+                // 2: 7 - 0 =  7
+                // ...
+                //
+                // (8_week - 3_wash) = 5_move
+                //
+                // weeks >= 7 / 5 == 2
+                //
+                // 2 + 1 = 3 weeks
                 3);
             Assert.AreEqual(3, numWeeks);
         }
