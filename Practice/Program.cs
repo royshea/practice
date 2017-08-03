f8fc295f37615e2ea381dbcb223e23285cc70fb5
Roy Shea <royshea@gmail.com>
2017-08-02 19:33:55 -0700

    main: Explicitly note that main is a stub.

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,13 +6,7 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Calling test");
-            var result = ANewHope.count(
-                new int[] { 1, 2, 3, 4 },
-                new int[] { 4, 3, 2, 1 },
-                3);
-            var returns = 4;
-            Console.WriteLine($"Observed {result} when expecting {returns}");
+            Console.WriteLine("Stub!");
         }
     }
 }
